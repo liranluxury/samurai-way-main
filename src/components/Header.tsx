e536ecfda9be3926f751e8a72912c9eae4f2f4df
liranluxury <lirangraf@gmail.com>
2024-03-27 09:56:29 +0200

    my third commit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ import classes from './Header.module.css';
 const Header = () => {
     return (
         <header className={classes.header}>
-            <img src={myLogo} alt="My Logo" />
+            <img src={myLogo} alt="My Logo"/>
         </header>
     );
 }
